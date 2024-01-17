ca8dfbc0c241139553ad355dae399deeb57c375d
Nohkii <120151129+Nohkii@users.noreply.github.com>
2024-01-17 16:37:55 +0900

    [Bronze V] Title: A+B - 5, Time: 124 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -4,7 +4,6 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         int num1;
@@ -14,12 +13,9 @@ public class Main {
             num1 = Integer.parseInt(st.nextToken());
             num2 = Integer.parseInt(st.nextToken());
             if (num1 == 0 && num2 == 0) break;
-            sb.append(num1 + num2);
-            sb.append("\n");
+            sb.append(num1 + num2).append("\n");
         }
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
         br.close();
     }
 }
