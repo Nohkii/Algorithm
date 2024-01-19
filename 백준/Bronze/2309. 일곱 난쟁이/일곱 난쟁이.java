57e47d9d6b70ca524da4eb7c96a10f70c8280ca2
Nohkii <120151129+Nohkii@users.noreply.github.com>
2024-01-20 01:46:03 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,46 +1,35 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        // 9명에서 2명의 난쟁이를 뺀 수가 100일 때 찾기
+        int sum = 0;
         int[] array = new int[9];
         for (int i = 0; i < 9; i++) {
             array[i] = Integer.parseInt(br.readLine());
+            sum += array[i];
         }
+        int num1 = 0;
+        int num2 = 0;
 
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        loof: for (int i = 0; i < array.length - 6; i++) {
-            for (int j = i + 1; j < array.length - 5; j++) {
-                for (int k = j + 1; k < array.length - 4; k++) {
-                    for (int l = k + 1; l < array.length - 3; l++) {
-                        for (int m = l + 1; m < array.length - 2; m++) {
-                            for (int n = m + 1; n < array.length - 1; n++) {
-                                for (int o = n + 1; o < array.length; o++) {
-                                    if (array[i] + array[j] + array[k] + array[l] + array[m] + array[n] + array[o] == 100) {
-                                        arrayList.add(array[i]);
-                                        arrayList.add(array[j]);
-                                        arrayList.add(array[k]);
-                                        arrayList.add(array[l]);
-                                        arrayList.add(array[m]);
-                                        arrayList.add(array[n]);
-                                        arrayList.add(array[o]);
-                                        break loof;
-                                    }
-                                }
-                            }
-                        }
-                    }
+        loof:
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (sum - array[i] - array[j] == 100) {
+                    num1 = array[i];
+                    num2 = array[j];
+                    break loof;
                 }
             }
         }
-        int[] answer = arrayList.stream().mapToInt(i -> i).toArray();
-        Arrays.sort(answer);
-        for (int i = 0; i < answer.length; i++) {
-            if (i == answer.length - 1) System.out.print(answer[i]);
-            else System.out.println(answer[i]);
+        Arrays.sort(array);
+        for(int i : array){
+            if(i == num1 || i == num2){
+                continue;
+            }
+            System.out.println(i);
         }
     }
 }
