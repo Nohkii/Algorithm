778338c11e1c678442f3c28604d608b865900228
Nohkii <120151129+Nohkii@users.noreply.github.com>
2024-01-20 10:24:49 +0900

    [Bronze II] Title: 나머지, Time: 116 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        for(int i = 0; i <10; i++){
+            hashSet.add(Integer.parseInt(br.readLine()) % 42);
+        }
+        System.out.println(hashSet.size());
+        br.close();
+    }
+}
